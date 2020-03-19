18aadb0e8a0348bfcfdf271287724f2502be5c04
Alexander Entinger <cto@lxrobotics.com>
2020-03-19 13:51:56 +0100

    Fixing examples for Uno WiFi Rev 2

--- examples/WiFiEchoCallback/WiFiEchoCallback.ino
+++ examples/WiFiEchoCallback/WiFiEchoCallback.ino
@@ -13,7 +13,7 @@
 */
 
 #include <ArduinoMqttClient.h>
-#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT)
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
   #include <WiFiNINA.h>
 #elif defined(ARDUINO_SAMD_MKR1000)
   #include <WiFi101.h>
