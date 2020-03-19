ca874d271ff6ffc0749a07ad42cb32f05588c003
Alexander Entinger <cto@lxrobotics.com>
2020-03-19 13:21:59 +0100

    Adding MKR1000, NANO 33 IOT, and ESP8266 as CI build targets

---
+++ .github/workflows/install-python-wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+# This script is used as an alternate entrypoint to allow installing Python 3 (a dependency of
+# the ESP8266 core for Arduino) in the Docker container used by the compile-examples action
+
+# Install Python 3
+apt-get update && apt-get install -y python3
+
+Run the standard entrypoint script
+/entrypoint.sh "$@"
