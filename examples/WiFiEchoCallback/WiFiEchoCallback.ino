1d934560d5a58f7c4a136447284033a914a37f50
pennam <m.pennasilico@arduino.cc>
2023-07-25 16:40:24 +0200

    Fix build for new boards

--- examples/WiFiEchoCallback/WiFiEchoCallback.ino
+++ examples/WiFiEchoCallback/WiFiEchoCallback.ino
@@ -19,8 +19,12 @@
   #include <WiFi101.h>
 #elif defined(ARDUINO_ARCH_ESP8266)
   #include <ESP8266WiFi.h>
-#elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_GIGA)
+#elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_GIGA) || defined(ARDUINO_OPTA)
   #include <WiFi.h>
+#elif defined(ARDUINO_PORTENTA_C33)
+  #include <WiFiC3.h>
+#elif defined(ARDUINO_UNOR4_WIFI)
+  #include <WiFiS3.h>
 #endif
 
 #include "arduino_secrets.h"
